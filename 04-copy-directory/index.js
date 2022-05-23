f840a655cf92ff0b0c0a0275623be2f764cf9c31
Anastasia <lireien@gmail.com>
2022-05-23 18:47:03 +0300

    feat: add directory copy solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require('fs');
+const path = require('path');
 
+//create original and destination path
+const copyFromPath = path.join(__dirname, 'files');
+const copyToPath = path.join(__dirname, 'files-copy');
+
+//create destination folder
+fs.mkdir(copyToPath, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+//read copyFolder content 
+fs.readdir(copyToPath, (err, data) => {
+  if (err) throw err;
+  for (let i = 0; i < data.length; i++) {
+    fs.unlink(`${copyToPath}/${data[i]}`, err => {
+      if (err) throw err;
+    });
+  }
+});
+//read mainFolder content and update copyFolder with it
+fs.readdir(copyFromPath, (err, data) => {
+  if (err) throw err;
+  for (let i = 0; i < data.length; i++) {
+    fs.copyFile(
+      `${copyFromPath}/${data[i]}`,
+      `${copyToPath}/${data[i]}`,
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+});
